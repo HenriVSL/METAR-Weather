fdb24f96fc992bd4c06b1bebf5bfcafe7d0f3988
Henri Lavikainen <henri.lavikainen@gmail.com>
2026-06-24 22:17:31 +0300

    Initial Commit

---
+++ METAR Weather/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  METAR Weather
+//
+//  Created by Henri Lavikainen on 24.6.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ METAR Weather/METAR_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  METAR_WeatherApp.swift
+//  METAR Weather
+//
+//  Created by Henri Lavikainen on 24.6.2026.
+//
+
+import SwiftUI
+
+@main
+struct METAR_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
